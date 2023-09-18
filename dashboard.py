1a8079c0aa776c31b85796dcf644a0206cfac9fa
Pisukeindo <145236948+Pisukeindo@users.noreply.github.com>
2023-09-19 02:07:58 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -5,16 +5,16 @@ from penjualan_harian import page2
 def db():
     # Sidebar
     st.sidebar.title("Menu Navigasi")
-    selected_page = st.sidebar.selectbox("Pilih Halaman:", ["Page1", "Page2"])
+    selected_page = st.sidebar.selectbox(["Halaman Input", "Bahan Baku Harian", "Penjualan Harian"])
 
     # Judul halaman
     st.title("Dashboard")
 
-    # Halaman Page1
-    if selected_page == "Page1":
+    # Halaman Bahan Baku Harian
+    if selected_page == "Bahan Baku Harian":
         page1()
 
-    # Halaman Page2
-    elif selected_page == "Page2":
+    # Halaman Penjualan Harian
+    elif selected_page == "Penjualan Harian":
         page2()
 
