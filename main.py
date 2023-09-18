fd429eda1e72b433992c9e6c837ea73502fe1219
Pisukeindo <145236948+Pisukeindo@users.noreply.github.com>
2023-09-19 02:22:16 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
 import streamlit as st
-from login_page import login
-from bahan_baku_harian import page1
-from penjualan_harian import page2
+import pandas as pd
+from dashboard import db
+
 # Inisialisasi status login
 if "username" not in st.session_state:
     st.session_state.username = None
 
-# Menampilkan halaman sesuai dengan status login
+# Halaman Login
 if st.session_state.username is None:
-    login()
+    st.title("Aplikasi Streamlit dengan Fitur Login")
+
+    # Load data akun
+    akun = "https://docs.google.com/spreadsheets/d/1Qs5LxFAcYkhVnAk0zAOBAu9FK8KVR8EKhg0Bl27DIk8/export?format=csv"
+    account_data = pd.read_csv(akun)
+
+    # Tambahkan input teks untuk username dan password
+    username = st.text_input("Username:")
+    password = st.text_input("Password:", type='password')
+
+    # Tombol untuk login
+    if st.button("Login"):
+        if not username or not password:
+            st.error("Isi kedua kolom username dan password.")
+        else:
+            # Periksa apakah kombinasi username dan password ada dalam data akun
+            user_data = account_data[(account_data['username'] == username) & (account_data['password'] == password)]
+
+            if user_data.empty:
+                st.error("Login gagal. Cek kembali username dan password Anda.")
+            else:
+                st.success("Login berhasil! Selamat datang, " + username)
+                st.session_state.username = user_data.iloc[0]['username']
+                
+
+# Halaman Dashboard
 else:
-    selected_page = st.selectbox("Pilih Halaman:", ["Bahan Baku Harian", "Penjualan Harian"])
-    if selected_page == "Bahan Baku Harian":
-      page1()
-    elif selected_page == "Penjualan Harian":
-      page2()
-  
+    db()
