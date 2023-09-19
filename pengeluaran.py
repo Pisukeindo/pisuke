fb0db1a1f3dfc48811312e6481f5b3cd9858d7af
Pisukeindo <145236948+Pisukeindo@users.noreply.github.com>
2023-09-19 16:56:01 +0700

    Update pengeluaran.py

--- pengeluaran.py
+++ pengeluaran.py
@@ -21,12 +21,13 @@ def page7():
     tanggal = st.date_input("Tanggal")
     tanggal_str = tanggal.strftime('%Y-%m-%d')
     sumber = st.text_input("Sumber")
-    st.write(f"Jumlah: {jumlah_rupiah}")
     jumlah = st.number_input("Jumlah (Rupiah)", min_value=0, format="%.0f")
+    jumlah_rupiah = format_rupiah(jumlah)
+    st.write(f"Jumlah: {jumlah_rupiah}")
     keterangan = st.text_input("Keterangan")
 
     # Mengubah jumlah menjadi format Rupiah
-    jumlah_rupiah = format_rupiah(jumlah)
+    
 
     if st.button("Kirim Data"):
         # Membangun URL dengan parameter query string
