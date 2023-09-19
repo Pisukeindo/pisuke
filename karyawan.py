aba9e969f967277fcbc6c33aa762e558566c8585
Pisukeindo <145236948+Pisukeindo@users.noreply.github.com>
2023-09-19 17:30:14 +0700

    Update karyawan.py

--- karyawan.py
+++ karyawan.py
@@ -2,6 +2,17 @@ import streamlit as st
 import requests
 
 def page6():
+
+    def format_rupiah(angka):
+        angka_str = str(angka)
+        ribuan = ""
+        n = len(angka_str)
+        for i, digit in enumerate(angka_str[::-1]):
+            ribuan = digit + ribuan
+            if (i + 1) % 3 == 0 and i != n - 1:
+                ribuan = "." + ribuan
+        return "Gaji: Rp " + ribuan
+        
     # URL layanan web Apps Script
     apps_script_url = "https://script.google.com/macros/s/AKfycbzNaLqTormZHk8lz9KEPiWxLUv1xp3-sxEXNELN8ejbNaWWT9O9j7EgkfgoBRfXp9NIvQ/exec"
     # Tampilan Streamlit
@@ -14,6 +25,9 @@ def page6():
     kontak = st.text_input("Kontak")
     alamat = st.text_input("Alamat")
     gaji = st.number_input("Gaji", min_value=0)
+    jumlah_rupiah = format_rupiah(gaji)
+    st.write(f"{jumlah_rupiah}")
+    
     tanggal = st.date_input("Tanggal")
     tanggal_str = tanggal.strftime('%Y-%m-%d')
 
