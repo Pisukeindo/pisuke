44187c4f53a9e1886876e9480be5af81001016c2
Pisukeindo <145236948+Pisukeindo@users.noreply.github.com>
2023-09-19 15:01:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -15,24 +15,33 @@ if "username" not in st.session_state:
 if st.session_state.username is None:
     login()
 else:
-    selected_page = st.sidebar.radio(
-        "Pilih Halaman:",
-        ["Dashboard", "tes"],
+    # Radio button untuk memilih "Halaman" atau "Laporan"
+    selected_option = st.sidebar.radio(
+        "Pilih Menu:",
+        ["Halaman", "Laporan"]
     )
 
-    if selected_page == "Dashboard":
-        page1()
-    elif selected_page == "tes":
-        page2()
+    if selected_option == "Halaman":
+        # Radio button untuk memilih halaman
+        selected_page = st.sidebar.radio(
+            "Pilih Halaman:",
+            ["Dashboard", "tes"]
+        )
 
-# Tambahkan CSS untuk menghilangkan bullet
-st.markdown(
-    """
-    <style>
-    .streamlit-radio-container input[type="radio"] + label::before {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
+        if selected_page == "Dashboard":
+            page1()
+        elif selected_page == "tes":
+            page2()
+    else:
+        # Radio button untuk memilih jenis laporan
+        selected_report = st.sidebar.radio(
+            "Pilih Laporan:",
+            ["Laporan QC", "Laporan Penjualan"]
+        )
+        
+        if selected_report == "Laporan QC":
+            st.title("Laporan Quality Control")
+            laporan("qc")
+        elif selected_report == "Laporan Penjualan":
+            st.title("Laporan Penjualan Harian")
+            laporan("penjualan_harian")
