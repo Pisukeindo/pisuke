b31b2bab11fd687d7b6e69b86be5182e9889a4e9
Pisukeindo <145236948+Pisukeindo@users.noreply.github.com>
2023-09-19 15:21:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
 import streamlit as st
+from login import login
+from suplier import page1
+from quality_control import page2
+from penjualan_harian import page3
+from pertambahan_aset import page4
+from bahan_baku_harian import page5
+from output import laporan
 
-# Header pada sidebar
-st.sidebar.header('Header Sidebar')
+# Inisialisasi status login
+if "username" not in st.session_state:
+    st.session_state.username = None
 
-# Sub-header pada sidebar
-st.sidebar.subheader('Sub-header Sidebar')
+# Menampilkan halaman sesuai dengan status login
+if st.session_state.username is None:
+    login()
+else:
+    selected_menu = st.sidebar.selectbox(
+        "Pilih Menu:",
+        ["Input Data", "Laporan"]
+    )
 
-# Konten lain pada sidebar
-st.sidebar.text('Ini adalah konten lain pada sidebar.')
+    if selected_menu == "Input Data":
+        selected_page = st.sidebar.radio(
+            "Pilih Halaman:",
+            ["Suplier", "Quality Control"]
+        )
 
-# Contoh submenu
-submenu_expander = st.sidebar.beta_expander("Submenu")
-with submenu_expander:
-    st.write("Konten submenu pertama")
-    st.write("Konten submenu kedua")
+        if selected_page == "Suplier":
+            page1()
+        elif selected_page == "Quality Control":
+            page2()
+    
+    elif selected_menu == "Laporan":
+        selected_laporan = st.sidebar.radio(
+            "Pilih Laporan:",
+            ["Laporan QC", "Laporan Suplier"]  # Ganti dengan nama laporan yang sesuai
+        )
+
+        if selected_laporan == "Laporan QC":
+            laporan("qc")  # Memanggil fungsi laporan dengan parameter yang sesuai
+        elif selected_laporan == "Laporan Suplier":
+            laporan("suplier")  # Memanggil fungsi laporan dengan parameter yang sesuai
