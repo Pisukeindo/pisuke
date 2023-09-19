f0c026d41355d3d18f24f8d1c1a9ec13060ef2a0
Pisukeindo <145236948+Pisukeindo@users.noreply.github.com>
2023-09-19 15:08:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -11,43 +11,54 @@ from output import laporan
 if "username" not in st.session_state:
     st.session_state.username = None
 
+# Inisialisasi status halaman aktif
+if "active_page" not in st.session_state:
+    st.session_state.active_page = None
+
 # Menampilkan halaman sesuai dengan status login
 if st.session_state.username is None:
     login()
 else:
-    # Sidebar dengan dua menu "PAGE" dan "LAPORAN"
-    selected_menu = st.sidebar.radio(
-        "Pilih Menu:",
-        ["PAGE", "LAPORAN"]
-    )
+    # Tampilkan subheader "PAGE"
+    st.sidebar.subheader("PAGE")
 
-    if selected_menu == "PAGE":
-        # Submenu "PAGE" dengan radio button
+    # Submenu "PAGE" dengan radio button
+    if st.session_state.active_page != "LAPORAN":
         selected_page = st.sidebar.radio(
             "Pilih Halaman:",
             ["page1", "page2", "page3", "page4", "page5"]
         )
 
-        if selected_page == "page1":
-            page1()
-        elif selected_page == "page2":
-            page2()
-        elif selected_page == "page3":
-            page3()
-        elif selected_page == "page4":
-            page4()
-        elif selected_page == "page5":
-            page5()
-    elif selected_menu == "LAPORAN":
-        # Submenu "LAPORAN" dengan radio button
+        if selected_page != st.session_state.active_page:
+            st.session_state.active_page = selected_page
+
+    # Tampilkan subheader "LAPORAN"
+    st.sidebar.subheader("LAPORAN")
+
+    # Submenu "LAPORAN" dengan radio button
+    if st.session_state.active_page != "PAGE":
         selected_report = st.sidebar.radio(
             "Pilih Laporan:",
             ["Laporan QC", "Laporan Penjualan"]
         )
-        
-        if selected_report == "Laporan QC":
-            st.title("Laporan Quality Control")
-            laporan("qc")
-        elif selected_report == "Laporan Penjualan":
-            st.title("Laporan Penjualan Harian")
-            laporan("penjualan_harian")
+
+        if selected_report != st.session_state.active_page:
+            st.session_state.active_page = selected_report
+
+    # Tampilkan halaman atau laporan yang aktif
+    if st.session_state.active_page == "page1":
+        page1()
+    elif st.session_state.active_page == "page2":
+        page2()
+    elif st.session_state.active_page == "page3":
+        page3()
+    elif st.session_state.active_page == "page4":
+        page4()
+    elif st.session_state.active_page == "page5":
+        page5()
+    elif st.session_state.active_page == "Laporan QC":
+        st.title("Laporan Quality Control")
+        laporan("qc")
+    elif st.session_state.active_page == "Laporan Penjualan":
+        st.title("Laporan Penjualan Harian")
+        laporan("penjualan_harian")
