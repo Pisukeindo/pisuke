cccba4c6fc6f027aa86822efc7ceb021eb4ba0ae
Pisukeindo <145236948+Pisukeindo@users.noreply.github.com>
2023-09-19 08:32:16 +0700

    Update penjualan_harian.py

--- penjualan_harian.py
+++ penjualan_harian.py
@@ -9,19 +9,23 @@ def page2():
     st.title("PENJUALAN HARIAN")
     st.write("Masukkan data:")
     
-    # Kolom input
-    tanggal = st.date_input("Tanggal")
-    tanggal_str = tanggal.strftime('%Y-%m-%d')
-    penjualan_pisang_aroma = st.number_input("Penjualan Pisang Aroma", min_value=0)
-    penjualan_cheese_roll = st.number_input("Penjualan Cheese Roll", min_value=0)
-    
-    # Pesan error jika ada input yang kosong
-    if not tanggal or not penjualan_pisang_aroma or not penjualan_cheese_roll:
-        st.error("Harap isi semua kolom sebelum mengirim data.")
-    else:
+    # Menggunakan st.form untuk mengelola formulir
+    with st.form("data_form"):
+        # Kolom input
+        tanggal = st.date_input("Tanggal")
+        penjualan_pisang_aroma = st.number_input("Penjualan Pisang Aroma", min_value=0)
+        penjualan_cheese_roll = st.number_input("Penjualan Cheese Roll", min_value=0)
         total_pendapatan = ((penjualan_pisang_aroma*1700)+(penjualan_cheese_roll*2000))
 
-        if st.button("Kirim Data"):
+        # Tombol untuk mengirimkan data
+        submit_button = st.form_submit_button("Kirim Data")
+    
+    # Verifikasi dan kirim data ketika tombol ditekan
+    if submit_button:
+        if not tanggal or not penjualan_pisang_aroma or not penjualan_cheese_roll:
+            st.error("Harap isi semua kolom sebelum mengirim data.")
+        else:
+            tanggal_str = tanggal.strftime('%Y-%m-%d')
             # Membangun URL dengan parameter query string
             url = f"{apps_script_url}?tanggal={tanggal_str}&penjualan_pisang_aroma={penjualan_pisang_aroma}&penjualan_cheese_roll={penjualan_cheese_roll}&total_pendapatan={total_pendapatan}"
         
@@ -32,3 +36,6 @@ def page2():
                 st.success("Data berhasil dikirim!")
             else:
                 st.error("Terjadi kesalahan saat mengirim data.")
+
+# Panggil fungsi page2 untuk menjalankan halaman
+page2()
