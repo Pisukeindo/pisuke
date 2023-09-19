382ab62fa1ff9b1455d860a2e6e7471779c70dd0
Pisukeindo <145236948+Pisukeindo@users.noreply.github.com>
2023-09-19 18:14:03 +0700

    Update output.py

--- output.py
+++ output.py
@@ -18,7 +18,7 @@ def format_tanggal(tanggal):
         # Jika format tanggal tidak valid, coba ekstrak tanggal dari format yang diberikan
         try:
             tanggal_obj = datetime.strptime(tanggal, '%Y-%m-%dT%H:%M:%S.%fZ')
-            tanggal_formatted = tanggal_obj.strftime('%y-%m-%d')
+            tanggal_formatted = tanggal_obj.strftime('%Y-%m-%d')
             return tanggal_formatted
         except Exception as e:
             return tanggal  # Kembalikan tanggal asli jika ada kesalahan
