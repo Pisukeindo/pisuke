023c8ad129b32050eecfe4c9bf690e24ba1138ef
Pisukeindo <145236948+Pisukeindo@users.noreply.github.com>
2023-09-19 09:35:16 +0700

    Create login.py

---
+++ login.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pandas as pd
+
+def login():
+    st.title("Aplikasi Streamlit dengan Fitur Login")
+
+    # Load data akun
+    akun = "https://docs.google.com/spreadsheets/d/1Qlt_9K0cTgAgdCoIvo9fEWnQi-QVokfAASNuECoOzpU/export?format=csv"
+    account_data = pd.read_csv(akun)
+
+    # Tambahkan input teks untuk username dan password
+    username = st.text_input("Username:")
+    password = st.text_input("Password:", type='password')
+
+    # Tombol untuk login
+    if st.button("Login"):
+        if not username or not password:
+            st.error("Isi kedua kolom username dan password.")
+            return
+
+        # Periksa apakah kombinasi username dan password ada dalam data akun
+        user_data = account_data[(account_data['username'] == username) & (account_data['password'] == password)]
+        
+        if user_data.empty:
+            st.error("Login gagal. Cek kembali username dan password Anda.")
+        else:
+            st.success("Login berhasil! Selamat datang, " + username)
+            st.session_state.username = user_data.iloc[0]['username']
