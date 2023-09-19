f2650fff16671e67c17ee12d401de3569e7ce53b
Pisukeindo <145236948+Pisukeindo@users.noreply.github.com>
2023-09-19 12:12:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 from login import login
 from suplier import page1
@@ -6,7 +7,6 @@ from penjualan_harian import page3
 from pertambahan_aset import page4
 from bahan_baku_harian import page5
 from output import laporan
-
 # Inisialisasi status login
 if "username" not in st.session_state:
     st.session_state.username = None
@@ -15,28 +15,27 @@ if "username" not in st.session_state:
 if st.session_state.username is None:
     login()
 else:
-    st.sidebar.title("INPUT DATA")
-
-    # Menu "INPUT DATA"
-    selected_page = st.sidebar.radio("", ["Penjualan Harian", "Quality Control", "Suplier", "Pertambahan Aset", "Bahan Baku Harian"])
-    if selected_page == "Penjualan Harian":
-        page3()
-    elif selected_page == "Quality Control":
-        page2()
+    selected_page = st.sidebar.selectbox("Pilih Halaman:", ["Dashboard", "Suplier", "Quality Control", 
+                                                            "Penjualan Harian", "Pertambahan Aset", "Bahan Baku Harian", "Laporan QC", "Laporan Suplier", "Laporan Penjualan Harian"])
+    if selected_page == "Dashboard":
+        st.write("Ini adalah halaman Dashboard.")
     elif selected_page == "Suplier":
-        page1()
+        page1() 
+    elif selected_page == "Quality Control":
+        page2() 
+    elif selected_page == "Penjualan Harian":
+        page3() 
     elif selected_page == "Pertambahan Aset":
-        page4()
+        page4() 
     elif selected_page == "Bahan Baku Harian":
-        page5()
-
-    st.sidebar.title("LAPORAN")
-
-    # Menu "LAPORAN"
-    selected_page = st.sidebar.radio("", ["Laporan QC", "Laporan Penjualan"])
-    if selected_page == "Laporan QC":
+        page5() 
+    elif selected_page == "Laporan QC":
         st.title("Laporan Quality Control")
-        laporan("qc")
-    elif selected_page == "Laporan Penjualan":
+        laporan("qc") 
+    elif selected_page == "Laporan Suplier":
+        st.title("Laporan Suplier")
+        laporan("suplier") 
+    elif selected_page == "Laporan Penjualan Harian":
         st.title("Laporan Penjualan Harian")
-        laporan("penjualan_harian")
+        laporan("penjualan_harian") 
+
