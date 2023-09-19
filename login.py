27aa2a7e546222c979551f5dd170bd13ef06c77a
Pisukeindo <145236948+Pisukeindo@users.noreply.github.com>
2023-09-19 10:08:45 +0700

    Update login.py

--- login.py
+++ login.py
@@ -1,14 +1,14 @@
 import streamlit as st
-import pandas as pd
 
-def login():
-    st.title("Aplikasi Streamlit dengan Fitur Login")
-
-    account_data = {
+# Buat dictionary untuk menyimpan data akun
+account_data = {
     'tes': 'tes',
     'user2': 'password2',
     'user3': 'password3'
 }
+
+def login():
+    st.title("Aplikasi Streamlit dengan Fitur Login")
 
     # Tambahkan input teks untuk username dan password
     username = st.text_input("Username:")
@@ -21,10 +21,10 @@ def login():
             return
 
         # Periksa apakah kombinasi username dan password ada dalam data akun
-        user_data = account_data[(account_data['username'] == username) & (account_data['password'] == password)]
-        
-        if user_data.empty:
-            st.error("Login gagal. Cek kembali username dan password Anda.")
-        else:
+        if username in account_data and account_data[username] == password:
             st.success("Login berhasil! Selamat datang, " + username)
-            st.session_state.username = user_data.iloc[0]['username']
+            st.session_state.username = username
+            st.experimental_rerun()
+        else:
+            st.error("Login gagal. Cek kembali username dan password Anda.")
+
