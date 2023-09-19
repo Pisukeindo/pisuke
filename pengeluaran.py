a166568a7212cb7f9efd17297222340c2f9b1e4d
Pisukeindo <145236948+Pisukeindo@users.noreply.github.com>
2023-09-19 16:42:50 +0700

    Update pengeluaran.py

--- pengeluaran.py
+++ pengeluaran.py
@@ -1,32 +1,13 @@
 import streamlit as st
-import requests
 
-def page7():
-    # URL layanan web Apps Script
-    apps_script_url = "https://script.google.com/macros/s/AKfycbxNLNOEH6spI2cGXRcufQ3sFKQUxgQf2F1QFrxtgXNwflEhuFdZy8iHzQM0yntI605t/exec"
-    # Tampilan Streamlit
-    st.title("PENGELUARAN")
-  
-    # Kolom input
-    tanggal = st.date_input("Tanggal")
-    tanggal_str = tanggal.strftime('%Y-%m-%d')
-    sumber = st.text_input("Sumber")
-    jumlah = st.number_input("Jumlah", min_value=0)
-    keterangan = st.text_input("Keterangan")
-    
-    
+# Tampilan Streamlit
+st.title("PENGELUARAN")
 
-    if st.button("Kirim Data"):
-        # Membangun URL dengan parameter query string
-        url = f"{apps_script_url}?sumber={sumber}&keterangan={keterangan}&jumlah={jumlah}&tanggal={tanggal_str}"
+# Kolom input
+tanggal = st.date_input("Tanggal")
+sumber = st.text_input("Sumber")
+jumlah = st.number_input("Jumlah (Rupiah)", min_value=0)
+keterangan = st.text_input("Keterangan")
 
-        # Mengirim permintaan HTTP GET ke Apps Script
-        response = requests.get(url)
-        
-        if response.status_code == 200:
-            st.success("Data berhasil dikirim!")
-        else:
-            st.error("Terjadi kesalahan saat mengirim data.")
-
-if __name__ == "__main__":
-    page7()
+# Menampilkan jumlah dalam format Rupiah secara real-time
+st.write(f"Jumlah (Rupiah): {jumlah:,.0f}")
