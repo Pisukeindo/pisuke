b4dc23740ff9f6ade564a06b66a1b9f1d7daa926
Pisukeindo <145236948+Pisukeindo@users.noreply.github.com>
2023-09-19 17:55:17 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import streamlit as st
 from login import login
 from suplier import page1
-from quality_control import page2
-from penjualan_harian import page3
 from pertambahan_aset import page4
 from bahan_baku_harian import page5
+from quality_control import page2
+from penjualan_harian import page3
 from karyawan import page6
-from pengeluaran_harian import page7
+from pengeluaran import page7
 from output import laporan
 
 # Inisialisasi status login
@@ -19,39 +19,47 @@ if st.session_state.username is None:
 else:
     selected_menu = st.sidebar.radio(
         "MENU:",
-        ["Input Data", "Laporan"]
+        ["Harian", "Input Data", "Laporan"]
     )
 
-    if selected_menu == "Input Data":
+    if selected_menu == "Harian":
+        selected_page = st.sidebar.radio(
+            "Input Harian:",
+            ["Quality Control", "Penjualan Harian", "Bahan Baku Harian", "Pengeluaran Harian"]
+        )
+
+        if selected_page == "Quality Control":
+            page2()
+        elif selected_page == "Penjualan Harian":
+            page3()
+        elif selected_page == "Bahan Baku Harian":
+            page5()
+        elif selected_page == "Pengeluaran Harian":
+            page7()
+
+    elif selected_menu == "Input Data":
         selected_page = st.sidebar.radio(
             "Input Data:",
-            ["Suplier", "Quality Control", "Penjualan Harian", "Pertambahan Aset", "Bahan Baku Harian", "Karyawan", "Pengeluaran Harian"]
+            ["Suplier", "Pertambahan Aset", "Karyawan"]
         )
 
         if selected_page == "Suplier":
             page1()
-        elif selected_page == "Quality Control":
-            page2()
-        elif selected_page == "Penjualan Harian":
-            page3()
         elif selected_page == "Pertambahan Aset":
             page4()
-        elif selected_page == "Bahan Baku Harian":
-            page5()
         elif selected_page == "Karyawan":
             page6()
-        elif selected_page == "Pengeluaran Harian":
-            page7()
-    
+        
+
     elif selected_menu == "Laporan":
         selected_laporan = st.sidebar.radio(
             "Laporan:",
-            ["Laporan QC", "Laporan Suplier", "Data Karyawan"]  # Ganti dengan nama laporan yang sesuai
+            ["Laporan QC", "Laporan Suplier", "Laporan Karyawan"]
         )
 
         if selected_laporan == "Laporan QC":
-            laporan("qc")  # Memanggil fungsi laporan dengan parameter yang sesuai
+            laporan("qc")
         elif selected_laporan == "Laporan Suplier":
-            laporan("suplier")  # Memanggil fungsi laporan dengan parameter yang sesuai
+            laporan("suplier")
         elif selected_laporan == "Data Karyawan":
-            laporan("karyawan")  # Memanggil fungsi laporan dengan parameter yang sesuai
+            laporan("karyawan")
