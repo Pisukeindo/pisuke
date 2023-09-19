0da777b2a90c74f2c8512807e15a823fdedd1c9b
Pisukeindo <145236948+Pisukeindo@users.noreply.github.com>
2023-09-19 12:40:54 +0700

    Update main.py

--- main.py
+++ main.py
@@ -11,29 +11,57 @@ from output import laporan
 if "username" not in st.session_state:
     st.session_state.username = None
 
+# Inisialisasi status sidebar
+if "show_sidebar" not in st.session_state:
+    st.session_state.show_sidebar = True
+
 # Menampilkan halaman sesuai dengan status login
 if st.session_state.username is None:
     login()
 else:
-    st.sidebar.title("INPUT DATA")
-    if st.sidebar.button("Penjualan Harian", key="penjualan_harian_button"):
-        page3()
-    if st.sidebar.button("Quality Control", key="qc_button"):
-        page2()
-    if st.sidebar.button("Suplier", key="suplier_button"):
-        page1()
-    if st.sidebar.button("Pertambahan Aset", key="pertambahan_aset_button"):
-        page4()
-    if st.sidebar.button("Bahan Baku Harian", key="bahan_baku_harian_button"):
-        page5()
+    if st.session_state.show_sidebar:
+        st.sidebar.title("MENU")
 
-    st.sidebar.title("LAPORAN")
-    if st.sidebar.button("Laporan QC", key="laporan_qc_button"):
-        st.title("Laporan Quality Control")
-        laporan("qc")
-    if st.sidebar.button("Laporan Penjualan", key="laporan_penjualan_button"):
-        st.title("Laporan Penjualan Harian")
-        laporan("penjualan_harian")
+        # Submenu "INPUT DATA"
+        submenu_input_data = st.sidebar.subheader("INPUT DATA")
+        submenu_input_data_options = {
+            "Suplier": page1,
+            "Quality Control": page2,
+            "Penjualan Harian": page3,
+            "Pertambahan Aset": page4,
+            "Bahan Baku Harian": page5,
+        }
+
+        # Submenu "LAPORAN"
+        submenu_laporan = st.sidebar.subheader("LAPORAN")
+        submenu_laporan_options = {
+            "Laporan QC": lambda: laporan("qc"),
+            "Laporan Penjualan": lambda: laporan("penjualan_harian"),
+        }
+
+        selected_page = None
+
+        for option in submenu_input_data_options:
+            if st.sidebar.button(option):
+                selected_page = option
+                st.session_state.show_sidebar = False  # Menyembunyikan sidebar setelah memilih submenu
+
+        for option in submenu_laporan_options:
+            if st.sidebar.button(option):
+                selected_page = option
+                st.session_state.show_sidebar = False  # Menyembunyikan sidebar setelah memilih submenu
+
+        if selected_page:
+            st.title(selected_page)
+            if selected_page in submenu_input_data_options:
+                submenu_input_data_options[selected_page]()
+            elif selected_page in submenu_laporan_options:
+                submenu_laporan_options[selected_page]()
+
+    else:
+        # Tombol untuk menampilkan kembali sidebar
+        if st.button("Tampilkan Sidebar"):
+            st.session_state.show_sidebar = True
 
 # Tambahkan CSS untuk menghilangkan persegi di sekitar tombol
 st.markdown(
