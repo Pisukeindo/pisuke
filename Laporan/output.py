4f94b0ff8e88dcb54ec43453b7c9156179a63326
Pisukeindo <145236948+Pisukeindo@users.noreply.github.com>
2023-11-18 23:43:44 +0700

    Update output.py

--- Laporan/output.py
+++ Laporan/output.py
@@ -9,8 +9,8 @@ google_apps_script_url = "https://script.google.com/macros/s/AKfycbwr-2CQmea3643
 # Fungsi untuk mengubah format tanggal menjadi "yyyy-mm-dd"
 def format_tanggal(tanggal):
     try:
-        # Ubah string tanggal ke dalam objek datetime
-        tanggal_obj = datetime.fromisoformat(tanggal)
+        # Ubah string tanggal ke dalam objek datetime dengan menyertakan zona waktu UTC
+        tanggal_obj = datetime.fromisoformat(tanggal + '+00:00')
         # Ubah format tanggal menjadi "yyyy-mm-dd"
         tanggal_formatted = tanggal_obj.strftime('%Y-%m-%d')
         print(f"Original: {tanggal}, Formatted: {tanggal_formatted}")
@@ -19,10 +19,12 @@ def format_tanggal(tanggal):
         # Jika format tanggal tidak valid, coba ekstrak tanggal dari format yang diberikan
         try:
             tanggal_obj = datetime.strptime(tanggal, '%Y-%m-%dT%H:%M:%S.%fZ')
+            tanggal_obj = tanggal_obj.replace(tzinfo=timezone.utc)
             tanggal_formatted = tanggal_obj.strftime('%Y-%m-%d')
             return tanggal_formatted
         except Exception as e:
             return tanggal  # Kembalikan tanggal asli jika ada kesalahan
+
 
 # Fungsi untuk mengubah angka menjadi format Rupiah
 def format_rupiah(angka):
