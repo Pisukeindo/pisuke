1e1e00e31de64ae9fe561b1101c97858ff03cdf3
Pisukeindo <145236948+Pisukeindo@users.noreply.github.com>
2023-11-18 23:59:26 +0700

    Create lap_karyawan.py

---
+++ Laporan/lap_karyawan.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import requests
+import pandas as pd
+
+def fetch_data_from_google_script():
+    # Ganti URL dengan URL Google Apps Script Anda
+    google_apps_script_url = "https://script.google.com/macros/s/AKfycbx14paFLfnGmvGtwR7-Cbjh5CxPrK9puiC_ofNUnlMH/dev"
+    # Mengirim permintaan HTTP ke Apps Script
+    response = requests.get(google_apps_script_url)
+    
+    if response.status_code == 200:
+        # Mengonversi data JSON menjadi DataFrame
+        data = response.json()
+        df = pd.DataFrame(data[1:], columns=data[0])
+        return df
+    else:
+        st.error("Gagal mengambil data dari Google Apps Script.")
+        return None
+
+def main():
+    st.title("Aplikasi Streamlit untuk Menampilkan Data dari Google Apps Script")
+
+    # Tombol untuk mengambil dan menampilkan data
+    if st.button("Tampilkan Data"):
+        data = fetch_data_from_google_script()
+        
+        # Jika data berhasil diambil, tampilkan dalam bentuk tabel
+        if data is not None:
+            st.write(data)
+
+if __name__ == "__main__":
+    main()
